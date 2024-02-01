91e066bff00a7af8e84c0cf630d60718b247d49c
Insomniac05 <116938601+Insomniac05@users.noreply.github.com>
2024-02-01 20:19:12 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -2,7 +2,7 @@
 
 mkdir unpack
 cd unpack
-../magiskboot unpack ../r.img
+../magiskboot unpack ../recovery.img
 ../magiskboot cpio ramdisk.cpio extract
 # Reverse fastbootd ENG mode check
 ~runner/work/Patch-Recovery/Patch-Recovery/magiskboot hexpatch system/bin/recovery e10313aaf40300aa6ecc009420010034 e10313aaf40300aa6ecc0094 # 20 01 00 35
@@ -19,5 +19,5 @@ cd unpack
 ~runner/work/Patch-Recovery/Patch-Recovery/magiskboot hexpatch system/bin/recovery b4f082ee28b1701c b4f082ee28b970c1
 ~runner/work/Patch-Recovery/Patch-Recovery/magiskboot hexpatch system/bin/recovery 9ef0f4ec28b1701c 9ef0f4ec28b9701c
 ~runner/work/Patch-Recovery/Patch-Recovery/magiskboot  cpio ramdisk.cpio 'add 0755 system/bin/recovery system/bin/recovery'
-~runner/work/Patch-Recovery/Patch-Recovery/magiskboot  repack ../r.img new-boot.img
+~runner/work/Patch-Recovery/Patch-Recovery/magiskboot  repack ../recovery.img new-boot.img
 cp new-boot.img ../recovery-patched.img
